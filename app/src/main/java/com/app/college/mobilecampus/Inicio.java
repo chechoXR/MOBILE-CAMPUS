ae4032d34047ec6db5ce0a3bc4f3496dab8f676c
Jaime Stiven Leon Tunjano <jaimeleonxd@gmail.com>
2019-10-09 18:05:47 -0500

    Creacion Vista Calendario y correcciones inicio
    Se creo la vista del calendario académico según los mockups, se arreglo la pantalla de inicio para que se pudieran ver todos los botones en cualquier pantalla agregando una scroll view, todas las pantallas ahora están bloqueadas en modo portrait.

--- app/src/main/java/com/app/college/mobilecampus/Inicio.java
+++ app/src/main/java/com/app/college/mobilecampus/Inicio.java
@@ -1,6 +1,8 @@
 package com.app.college.mobilecampus;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +14,26 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-        Button btn = findViewById(R.id.button_consultaSem);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btnSem = findViewById(R.id.button_consultaSem);
+        Button btnCal = findViewById(R.id.button_calendarioAcademico);
+        btnSem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (v.getContext(),Semilleros.class);
                 startActivity(intent);
             }
         });
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Calendario c1 = new Calendario();
+                fragmentTransaction.add(R.id.fragment_container,c1);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 }
