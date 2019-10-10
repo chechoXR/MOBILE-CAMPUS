fe0298000537958adc41d224ff83a2aed0d8600f
pipebrelinz <Felipe Barragan>
2019-10-09 23:37:35 -0500

    Vista Acerca de la App
    Se agrega vista sobre informacion basica de la app

---
+++ app/src/main/java/com/app/college/mobilecampus/AcercaDeLaApp.java
@@ -0,0 +1,20 @@
+package com.app.college.mobilecampus;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AcercaDeLaApp extends Fragment {
+
+    public AcercaDeLaApp (){
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_acerca_de_la_app, container, false);
+    }
+}
--- app/src/main/java/com/app/college/mobilecampus/Inicio.java
+++ app/src/main/java/com/app/college/mobilecampus/Inicio.java
@@ -1,6 +1,7 @@
 package com.app.college.mobilecampus;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -16,10 +17,12 @@ public class Inicio extends AppCompatActivity {
         setContentView(R.layout.activity_inicio);
         Button btnSem = findViewById(R.id.button_consultaSem);
         Button btnCal = findViewById(R.id.button_calendarioAcademico);
+        Button btnAcerca = findViewById(R.id.button_acerca_de_la_app);
+
         btnSem.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(),Semilleros.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Semilleros.class);
                 startActivity(intent);
             }
         });
@@ -29,11 +32,23 @@ public class Inicio extends AppCompatActivity {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 Calendario c1 = new Calendario();
-                fragmentTransaction.add(R.id.fragment_container,c1);
+                fragmentTransaction.add(R.id.fragment_container, c1);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
             }
         });
 
+        btnAcerca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AcercaDeLaApp acercaApp = new AcercaDeLaApp();
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.add(R.id.fragment_acerca_de_la_app, acercaApp);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
     }
+
 }
