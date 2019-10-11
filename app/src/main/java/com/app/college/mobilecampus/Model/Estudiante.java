fdb5cf13a750684afd3146dc36d76ea1df62a9b8
Camilo <camiloandrespiza@gmail.com>
2019-10-10 19:05:19 -0500

    Login funcional

---
+++ app/src/main/java/com/app/college/mobilecampus/Model/Estudiante.java
@@ -0,0 +1,45 @@
+package com.app.college.mobilecampus.Model;
+
+public class Estudiante {
+
+    private String nombre,apellido,correo,usuario;
+
+    public Estudiante(String nombre, String apellido, String correo, String usuario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
