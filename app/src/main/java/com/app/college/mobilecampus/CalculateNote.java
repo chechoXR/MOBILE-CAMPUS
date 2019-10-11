083794cd3a4b70c9c62a468fde1217ed3d2a49a3
DESKTOP-KVQ5F3E\JOSE LUIS NOVOA <jonovoan@poligran.edu.co>
2019-10-10 20:24:54 -0500

    Calculo notas
    Vista calcular notas

---
+++ app/src/main/java/com/app/college/mobilecampus/CalculateNote.java
@@ -0,0 +1,13 @@
+package com.app.college.mobilecampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculateNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_note);
+    }
+}
