078ecdc18cda36d13489088343ec788488de2d2a
Camilo <camiloandrespiza@gmail.com>
2019-10-16 03:38:04 -0500

    Se arregló bug de inicio de sesión erroneo
    +se agrego dependencia en gladle
    +se creo clase utils para generar toast personalizados, de esa manera reusar codigo. 
    +se modifico archivo styles para que al salir de la aplicacion se vea el color primario.
    +se modifico archivo manifest para dejar la aplicacion en modo portaretrato de manera fija.

--- app/src/main/java/com/app/college/mobilecampus/Login.java
+++ app/src/main/java/com/app/college/mobilecampus/Login.java
@@ -1,15 +1,12 @@
 package com.app.college.mobilecampus;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-
-import com.app.college.mobilecampus.Model.Estudiante;
 import com.app.college.mobilecampus.ServicesConsumer.LoginConsumer;
 
 public class Login extends AppCompatActivity {
@@ -37,18 +34,18 @@ public class Login extends AppCompatActivity {
         });
     }
 
-        private void checkUser(String username, String password){
+    private void checkUser(String username, String password){
 
         submit.setVisibility(View.INVISIBLE);
         progressBar.setVisibility(View.VISIBLE);
-            LoginConsumer loginConsumer = new LoginConsumer(this);
-            if(!username.equals("") && !password.equals("")){
-                loginConsumer.loginRequest(username,password);
-            }else {
-                submit.setVisibility(View.VISIBLE);
-                progressBar.setVisibility(View.GONE);
-                this.password.setText("");
-            }
-
+        LoginConsumer loginConsumer = new LoginConsumer(this);
+        if(!username.equals("") && !password.equals("")){
+            loginConsumer.loginRequest(username,password);
+        }else {
+            submit.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.GONE);
+            this.password.setText("");
         }
+
+    }
 }
