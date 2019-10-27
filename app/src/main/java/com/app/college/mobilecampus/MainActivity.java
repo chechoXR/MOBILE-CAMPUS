205bef84d5be269986a4f4b4f361dd67cb6e986d
Camilo <camiloandrespiza@gmail.com>
2019-10-27 12:56:59 -0500

    Cambio de configuracion de activity a fragment
    -arreglos de action bar

--- app/src/main/java/com/app/college/mobilecampus/MainActivity.java
+++ app/src/main/java/com/app/college/mobilecampus/MainActivity.java
@@ -1,9 +1,19 @@
 package com.app.college.mobilecampus;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -13,10 +23,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +69,18 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(MainActivity.this,Settings.class);
+                startActivity(intent);
+                break;
+        }
         return true;
     }
 
--- app/src/main/java/com/app/college/mobilecampus/ui/settings/FragmentSettingsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.app.college.mobilecampus.ui.settings;
-
-import androidx.lifecycle.ViewModel;
-
-public class FragmentSettingsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
