56c8232a4c220f8e7d87e9f4f0e1d615324c563d
pipebrelinz <Felipe Barragan>
2019-11-06 11:52:06 -0500

    Se agrega el id (codigo estudiante)
    Se agrega el atributo id al modelo estudiante y al servicio LoginConsumer

--- app/src/main/java/com/app/college/mobilecampus/model/Estudiante.java
+++ app/src/main/java/com/app/college/mobilecampus/model/Estudiante.java
@@ -2,13 +2,14 @@ package com.app.college.mobilecampus.model;
 
 public class Estudiante {
 
-    private String nombre,apellido,correo,usuario;
+    private String nombre,apellido,correo,usuario,id;
 
-    public Estudiante(String nombre, String apellido, String correo, String usuario) {
+    public Estudiante(String nombre, String apellido, String correo, String usuario, String id) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
         this.usuario = usuario;
+        this.id = id; //Codigo Estudiante
     }
 
     public String getNombre() {
@@ -42,4 +43,8 @@ public class Estudiante {
     public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
 }
