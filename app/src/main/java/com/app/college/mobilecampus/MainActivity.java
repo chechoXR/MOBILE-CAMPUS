64bc304b124d5641b9b0c738605c550138049080
Sergio Ramirez <sergioestebanramirez@yahoo.es>
2019-11-09 22:24:54 -0500

    Bug de llaves corregido (MainActivity)

--- app/src/main/java/com/app/college/mobilecampus/MainActivity.java
+++ app/src/main/java/com/app/college/mobilecampus/MainActivity.java
@@ -140,19 +140,19 @@ public class MainActivity extends AppCompatActivity {
         Navigation.findNavController(view).navigate(R.id.nav_todo);
     }
 
-    public static void moveFloatingButton(float Y, long time){
+    public static void moveFloatingButton(float Y, long time) {
 
 
-        if(time-lastMovementFloatingButton > 1000){
+        if (time - lastMovementFloatingButton > 1000) {
             fab.setTranslationY(Y);
 
-        lastMovementFloatingButton = System.currentTimeMillis();
+            lastMovementFloatingButton = System.currentTimeMillis();
         }
-    
 
-    public void calcularNota(View view){
-        Navigation.findNavController(view).navigate(R.id.nav_calcular_nota);
     }
+    
+        public void calcularNota (View view){
+            Navigation.findNavController(view).navigate(R.id.nav_calcular_nota);
+        }
 
-
-}
+    }
